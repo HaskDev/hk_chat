05b40b7e535aa48eb4dcff678463a97d9445d5d6
HaskDev <100434787+HaskDev@users.noreply.github.com>
2022-09-21 23:25:14 +0200

    Add files via upload

---
+++ chat/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+game 'gta5'
+author 'MYX'
+
+client_scripts {
+    'cl_chat.lua'
+}
+server_scripts {
+    'sv_chat.lua'
+}
+
+files {
+    '**/**/**/**/**/*.*'
+}
+
+ui_page {
+    'html/index.html'
+}
